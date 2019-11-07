3100f6fe2400d1d74d1c9f8dda8466a59290c3ea
Kasper Fabæch Brandt <poizan@poizan.dk>
2019-11-07 01:52:35 +0100

    Add project files.

---
+++ FileStreamPerformance/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Console = Internal.Console;
+
+namespace FileStreamPerformance
+{
+  class Program
+  {
+    [DllImport("Kernel32.dll", SetLastError = true)]
+    private static extern bool CreatePipe(out SafeFileHandle hReadPipe, out SafeFileHandle hWritePipe, IntPtr lpPipeAttributes, int nSize);
+
+    static void Main(string[] args)
+    {
+      if (!CreatePipe(out SafeFileHandle readPipeHandle, out SafeFileHandle writePipeHandle, IntPtr.Zero, 0))
+      {
+        Console.WriteLine(string.Format("CreatePipe failed: {0:X8}", Marshal.GetLastWin32Error()));
+        return;
+      }
+      Task benchmarkTask = new Task(() =>
+      {
+        // ...
+      }, TaskCreationOptions.LongRunning);
+      Thread writerThread = new Thread(() =>
+      {
+        using (FileStream fsWrite = new FileStream(writePipeHandle, FileAccess.Write))
+        {
+          byte[] buf = new byte[10240];
+          while (true)
+          {
+            try
+            {
+              fsWrite.Write(buf);
+            }
+            catch (IOException)
+            {
+              return;
+            }
+          }
+        }
+      });
+      writerThread.IsBackground = true;
+      writerThread.Start();
+      benchmarkTask.Wait();
+    }
+  }
+}
---
+++ TestPipeCancel/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Console = Internal.Console;
+
+namespace TestPipeCancel
+{
+  class Program
+  {
+    [DllImport("Kernel32.dll", SetLastError = true)]
+    private static extern bool CreatePipe(out SafeFileHandle hReadPipe, out SafeFileHandle hWritePipe, IntPtr lpPipeAttributes, int nSize);
+
+    static void Main(string[] args)
+    {
+      if (!CreatePipe(out SafeFileHandle readPipeHandle, out SafeFileHandle writePipeHandle, IntPtr.Zero, 0))
+      {
+        Console.WriteLine(string.Format("CreatePipe failed: {0:X8}", Marshal.GetLastWin32Error()));
+        return;
+      }
+      FileStream fs = new FileStream(readPipeHandle, FileAccess.Read, 4096, false);
+      byte[] buffer = new byte[1];
+      CancellationTokenSource cts = new CancellationTokenSource();
+      Task<int> asyncRead = fs.ReadAsync(buffer, 0, 1, cts.Token);
+      Thread.Sleep(1000);
+      cts.Cancel();
+      Task.WaitAny(asyncRead);
+      Console.WriteLine($"Status: {asyncRead.Status}");
+      Console.WriteLine($"Exception: {asyncRead.Exception}");
+      try
+      {
+        Console.WriteLine($"bytes read: {asyncRead.Result}");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"asyncRead.Result threw {e}");
+      }
+    }
+  }
+}
